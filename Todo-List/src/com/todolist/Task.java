63797b194b0e325f4e634507694aef0c310db342
Mohan <smohan0305@gmail.com>
2024-04-30 15:02:31 +0530

    taskNo variable is removed

--- Todo-List/src/com/todolist/Task.java
+++ Todo-List/src/com/todolist/Task.java
@@ -2,21 +2,16 @@ package com.todolist;
 
 public class Task {
 	// variables
-	private int taskNo;
 	private String taskName;
 	private String dueDate;
 	
 	// create constructor for task
-	public Task (int tNo, String tName, String dDate) {
-		this.taskNo = tNo;
+	public Task (String tName, String dDate) {
 		this.taskName = tName;
 		this.dueDate = dDate;
 	}
 	
 	// getters for access private variables
-	public int getTaskNo() {
-		return taskNo;
-	}
 	
 	public String getTaskName() {
 		return taskName;
@@ -29,7 +24,7 @@ public class Task {
 	// toString method to override the task variables
 	@Override
 	public String toString() {
-		return this.taskNo + " " + this.taskName + " " + this.dueDate;
+		return this.taskName + " " + this.dueDate;
 	}
 	
 }
