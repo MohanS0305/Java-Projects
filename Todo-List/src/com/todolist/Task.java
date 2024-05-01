866feeeac4e5ddd67475ac37c8417a671bd99a12
Mohan <smohan0305@gmail.com>
2024-05-01 11:19:47 +0530

    taskNo variable added

--- Todo-List/src/com/todolist/Task.java
+++ Todo-List/src/com/todolist/Task.java
@@ -2,29 +2,34 @@ package com.todolist;
 
 public class Task {
 	// variables
+	private int taskNo;
 	private String taskName;
 	private String dueDate;
-	
+
 	// create constructor for task
-	public Task (String tName, String dDate) {
+	public Task(int tNo, String tName, String dDate) {
+		this.taskNo = tNo;
 		this.taskName = tName;
 		this.dueDate = dDate;
 	}
-	
+
 	// getters for access private variables
-	
+	public int getTaskNo() {
+		return this.taskNo;
+	}
+
 	public String getTaskName() {
 		return taskName;
 	}
-	
+
 	public String getDueDate() {
 		return dueDate;
 	}
-	
+
 	// toString method to override the task variables
 	@Override
 	public String toString() {
-		return this.taskName + " " + this.dueDate;
+		return this.taskNo + " " + this.taskName + " " + this.dueDate;
 	}
-	
+
 }
