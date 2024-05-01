b3514479ce43f688405d3f86104c85682a82f0cf
Mohan <smohan0305@gmail.com>
2024-05-01 11:18:15 +0530

    add, remove and update task in separate method

--- Todo-List/src/com/todolist/Main.java
+++ Todo-List/src/com/todolist/Main.java
@@ -11,51 +11,38 @@ public class Main {
 			TodoList todoList = new TodoList();
 
 			// manually add some task
-			todoList.addTask(new Task("wake up 5'O clock", "2024-04-30"));
-			todoList.addTask(new Task("do some exercise", "2024-04-30"));
-			todoList.addTask(new Task("make breakfast", "2024-04-30"));
+			todoList.addTask(new Task(1, "wake up 5'O clock", "2024-04-30"));
+			todoList.addTask(new Task(2, "do some exercise", "2024-04-30"));
+			todoList.addTask(new Task(3, "make breakfast", "2024-04-30"));
 
 			// display the current task
 			System.out.println("Current Tasks :");
 			todoList.displayTasks();
+			
+			// create object for userTaskInput class
+			UserTaskInput uti = new UserTaskInput();
 
-			// ask choice for user want to add or remove the task and exit
-			System.out.println("\nEnter any one choice : 1, 2, 3 or 4");
-			System.out.println("1. Add the task \n 2. Remove the task \n 3. Display updated tasks \n 4. Exit");
-			int choice = input.nextInt();
+			// call the userChoice method ask the choice store it in choice
+			int choice = uti.userChoice();
 			while (choice > 0 && choice != 4) {
 				switch (choice) {
 				case 1 -> {
-					// ask the input for task from user end
-					// this nexLine() for skipping purpose
-					input.nextLine();
-					// ask taskName as input
-					System.out.println("Enter the task name :");
-					String taskName = input.nextLine();
-					// ask task dueDate as input
-					System.out.println("Enter the task due date :");
-					String dueDate = input.nextLine();
-					// add all this input in tasks array list
-					todoList.addTask(new Task(taskName, dueDate));
-					choice = 0;
+					uti.addTheTask(todoList); // call addTheTask method 
 				}
 				case 2 -> {
-					// ask the input for remove task
-					System.out.println("Enter the task number :");
-					int removeTask = input.nextInt();
-					todoList.removeTask(removeTask);
-					choice = 0;
+					uti.removeTheTask(todoList); // call removeTheTask method
 				}
 				case 3 -> {
 					// display the updated task
 					System.out.println("Updated Tasks :");
 					todoList.displayTasks();
-					choice = 0;
 				}
-				default -> System.out.println("Invalid choice");
+				default -> System.out.println("\nInvalid choice pls enter correct choice");
 				}
+				// after perform each operation ask the choice again
+				choice = uti.userChoice();
 			}
-			System.out.println("Thank you bye bye...");
+			System.out.println("\nThank you bye bye...");
 		}
 	}
 }
